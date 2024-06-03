b87714035b87478c6c3befa5c66b93d5cd7526d8
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-03 20:51:09 +0200

    building the pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Home from './pages/Home';
+import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
