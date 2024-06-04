e5d8dce9b30ffbff695148cd956adcce2999d86b
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-04 23:52:52 +0200

    consuming the cities context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -33,4 +33,11 @@ const CitiesProvider = ({ children }) => {
   );
 };
 
-export { CitiesProvider };
+const useCities = () => {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside of the CitiesProvider');
+  return context;
+};
+
+export { CitiesProvider, useCities };
