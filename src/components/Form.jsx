c9bc96ddb45b70972089b6111e1d741ba0d9d12b
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-05 19:00:51 +0200

    creating a city

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 import styles from './Form.module.css';
 import Button from './Button';
@@ -8,6 +11,8 @@ import BackButton from './BackButton';
 import { useUrlPosition } from '../hooks/useUrlPosition';
 import Message from './Message';
 import Spinner from './Spinner';
+import { useCities } from '../contexts/CitiesContext';
+import { useNavigate } from 'react-router-dom';
 
 export const convertToEmoji = (countryCode) => {
   const codePoints = countryCode
@@ -21,6 +26,9 @@ const BASE_URL = 'https://api.bigdatacloud.net/data/reverse-geocode-client';
 
 const Form = () => {
   const [lat, lng] = useUrlPosition();
+  const { createCity, isLoading } = useCities();
+
+  const navigate = useNavigate();
 
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState('');
@@ -31,6 +39,8 @@ const Form = () => {
   const [geocodingError, setGeocodingError] = useState('');
 
   useEffect(() => {
+    if (!lat && !lng) return;
+
     const fetchCityData = async () => {
       try {
         setIsLoadingGeocoding(true);
@@ -57,10 +67,33 @@ const Form = () => {
   }, [lat, lng]);
 
   if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng) return <Message message="Start by clicking on the map" />;
   if (geocodingError) return <Message message={geocodingError} />;
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    await createCity(newCity);
+    navigate('/app/cities');
+  };
+
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? 'loading' : ''}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -73,10 +106,11 @@ const Form = () => {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          selected={date}
+          onChange={(date) => setDate(date)}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -41,8 +41,30 @@ const CitiesProvider = ({ children }) => {
     }
   };
 
+  const createCity = async (newCity) => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+
+      setCities((cities) => [...cities, data]);
+    } catch (e) {
+      console.log('There was an error loading data...');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
+    <CitiesContext.Provider
+      value={{ cities, isLoading, currentCity, getCity, createCity }}
+    >
       {children}
     </CitiesContext.Provider>
   );
